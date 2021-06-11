e133be2e77a055ab3b82fd10d7d21f581d9c65ba
Di Silvestro Giacomo <giacomo>
2021-06-11 11:29:00 +0200

    Initial commit

---
+++ src/main/java/it/nttdataconcessionario/ConcessionarioApplication.java
@@ -0,0 +1,13 @@
+package it.nttdataconcessionario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcessionarioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcessionarioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/nttdataconcessionario/ConcessionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package it.nttdataconcessionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcessionarioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
